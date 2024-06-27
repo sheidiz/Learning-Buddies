6a9f849ae5ab90707309ee2f3d6fe0bd157a9621
Sheila Diz <dizsheila07@gmail.com>
2024-06-26 22:08:06 -0300

    feat: create base project

---
+++ learning-buddies-backend/src/test/java/com/sheiladiz/LearningBuddiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheiladiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBuddiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
